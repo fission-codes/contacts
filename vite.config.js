ab6a310e6fa7c4df9a6bdde2cd06cbbda93c723f
Steven Vandevelde <icid.asset@gmail.com>
2021-05-31 16:52:21 +0200

    Upgrade elm vite plugin

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,20 @@ import elmPlugin from "vite-plugin-elm"
 
 
 const elm = elmPlugin()
+const originalTransform = elm.transform
+
+
+elm.transform = function(...args) {
+  if (!args[1].endsWith(".elm")) return
+
+  console.log(execSync(`just css`).toString())
+  return originalTransform.apply(this, args)
+}
 
 
 module.exports = {
   plugins: [
-    {
-      ...elm,
-      transform: (...args) => {
-        if (!args[1].endsWith(".elm")) return
-
-        console.log(execSync(`just css`).toString())
-        return elm.transform(...args)
-      }
-    }
+    elm
   ],
 
   publicDir: "Static",
